b1c1920bc7cd59cef605c939090cc40d36101b97
Dodo0303 <dodo0303@icloud.com>
2022-03-24 17:54:14 +0100

    Added constructors to cloud concrete classes.

--- src/main/java/it/polimi/ingsw/CloudThreePlayers.java
+++ src/main/java/it/polimi/ingsw/CloudThreePlayers.java
@@ -1,6 +1,13 @@
 package it.polimi.ingsw;
 
+import java.util.ArrayList;
+
 public class CloudThreePlayers extends Cloud {
+
+    public CloudThreePlayers() {
+        this.students = new ArrayList<>();
+    }
+
     public void addStudents(StudentColors student) throws FullCloudException {
         if (getStudents().size() >= 4) throw new FullCloudException();
         else super.students.add(student);
--- src/main/java/it/polimi/ingsw/CloudTwoFourPlayers.java
+++ src/main/java/it/polimi/ingsw/CloudTwoFourPlayers.java
@@ -1,6 +1,13 @@
 package it.polimi.ingsw;
 
+import java.util.ArrayList;
+
 public class CloudTwoFourPlayers extends Cloud {
+
+    public CloudTwoFourPlayers() {
+        this.students = new ArrayList<>();
+    }
+
     public void addStudents(StudentColors student) throws FullCloudException {
         if (getStudents().size() >= 3) throw new FullCloudException();
         else super.students.add(student);
