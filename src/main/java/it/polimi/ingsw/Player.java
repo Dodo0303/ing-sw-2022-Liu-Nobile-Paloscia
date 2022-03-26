4aba284b57b9524564c42da98d983411897f3ce7
ihsinliu <ihsin.liu@outlook.com>
2022-03-26 11:01:42 +0100

    update player.java

--- src/main/java/it/polimi/ingsw/Player.java
+++ src/main/java/it/polimi/ingsw/Player.java
@@ -1,9 +1,9 @@
 package it.polimi.ingsw;
 
 public abstract class Player {
+
     /** A Player in GAME, initially playing COLOR. */
-    Player(Game game, Color color) {
-        _game = game;
+    Player(Color color) {
         _color = color;
         //TODO
     }
@@ -13,14 +13,7 @@ public abstract class Player {
         return _color;
     }
 
-    /** Return the Game I am currently playing in. */
-    final Game getGame() {
-        return _game;
-    }
-
     /** My current color. */
     private Color _color;
 
-    /** The game I'm in. */
-    private final Game _game;
 }
