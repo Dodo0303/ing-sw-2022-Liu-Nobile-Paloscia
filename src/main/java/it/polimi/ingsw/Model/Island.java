21ca3d141680016e31325a9a90be4361f9aaf103
ihsinliu <ihsin.liu@outlook.com>
2022-03-29 17:01:05 +0200

    update Island.java

--- src/main/java/it/polimi/ingsw/Model/Island.java
+++ src/main/java/it/polimi/ingsw/Model/Island.java
@@ -4,14 +4,17 @@ import java.util.HashMap;
 
 public class Island {
 
-
+    /** A new island.
+     */
     Island() {
         _towerColor = Color.VOID;
         _noEntries = 0;
         _numMerge = 0; //how many times THIS island has merged other islands into itself, also equals to the number of towers on THIS island.
-        students = new HashMap<>();
+        _students = new HashMap<>();
+        _influences = new HashMap<>();
         for(StudentColor color : StudentColor.values()) {
-            students.put(color, 0);
+            _students.put(color, 0);
+            _influences.put(color, 0);
         }
     }
 
@@ -19,26 +22,43 @@ public class Island {
      * @param x index of the island.
      */
     void copyFrom(Island x) {
-        _numMerge = this._numMerge + x._numMerge + 1;
-        for (StudentColor color : x.students.keySet()) {
-            this.students.put(color, x.students.get(color) + this.students.get(color));
+        if (this._towerColor == x._towerColor) {
+            _numMerge = this._numMerge + x._numMerge + 1;
+            for (StudentColor color : x._students.keySet()) {
+                this._students.put(color, x._students.get(color) + this._students.get(color));
+            }
+        } else {
+            throw new GameException("You cannot unify islands with different tower colors.");
         }
 
     }
-
+    /** Add a student of color COLOR to THIS island.
+     * @param color the color of the student to be added to THIS island.
+     */
     void addStudent(StudentColor color) {
-        students.put(color, students.get(color) + 1);
+        _students.put(color, _students.get(color) + 1);
+    }
+
+    /** Set color of the tower(s) on this island to color COLOR.
+     */
+    void setTowerColor(Color color) {
+        _towerColor = color;
     }
 
 
-    private int[] _influences;
+    /** Influence of each color. */
+    private HashMap<StudentColor, Integer> _influences;
 
+    /** The color of tower(s) on THIS island. */
     private Color _towerColor;
 
+    /** //TODO */
     private int _noEntries;
 
+    /** Represents how many times THIS island has merged other islands into itself, also equals to the number of towers on THIS island. */
     private int _numMerge;
 
-    private HashMap<StudentColor, Integer> students;
+    /** Students on THIS island counted by color. */
+    private HashMap<StudentColor, Integer> _students;
 
 }
