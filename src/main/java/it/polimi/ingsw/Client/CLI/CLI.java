d09ad698bfa736b7f1abb1f6fcd313ce85133688
ihsinliu <ihsin.liu@outlook.com>
2022-05-09 22:44:19 +0200

    glitch flixed

--- src/main/java/it/polimi/ingsw/Client/CLI/CLI.java
+++ src/main/java/it/polimi/ingsw/Client/CLI/CLI.java
@@ -51,7 +51,7 @@ public class CLI {
         serverHandlerThread.start();
         requireNickname();
         chooseGameMode();
-        while (closed) {
+        while (!closed) {
             playAssistant();//planning
             moveStudentsFromEntrance();//AP1
             moveMotherNature();//AP2
