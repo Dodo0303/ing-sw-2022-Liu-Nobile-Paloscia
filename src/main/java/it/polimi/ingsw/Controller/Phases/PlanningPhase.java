5c4aac630eac6a0c1dc2244f9b036f6e5becfffb
ihsinliu <ihsin.liu@outlook.com>
2022-05-17 18:22:11 +0200

    The issue of cloud being empty was solved by the previous commit as well.

--- src/main/java/it/polimi/ingsw/Controller/Phases/PlanningPhase.java
+++ src/main/java/it/polimi/ingsw/Controller/Phases/PlanningPhase.java
@@ -16,8 +16,6 @@ public class PlanningPhase extends Phase {
         super(match);
 
         //TODO: What if bag is empty?
-        //TODO: might forget to extract students from bag before using fillClouds()?
-        //TODO: need to fill the cloud before the 2nd, 3rd...nth round starts
 
         this.match.fillClouds();
         this.match.broadcastClouds();
