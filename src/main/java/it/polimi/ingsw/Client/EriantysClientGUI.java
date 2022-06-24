e1d2f2cebe2d01b2db97d9769547c395dac40997
ihsinliu <ihsin.liu@outlook.com>
2022-06-24 21:18:41 +0200

    Disable system scaling

--- src/main/java/it/polimi/ingsw/Client/EriantysClientGUI.java
+++ src/main/java/it/polimi/ingsw/Client/EriantysClientGUI.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 public class EriantysClientGUI extends Application {
 
     public static void main(String[] args) {
+        System.setProperty("prism.allowhidpi", "false");
         launch(args);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-        System.setProperty("sun.java2d.uiScale", "1.0");
         GUI gui = new GUI(stage);
         gui.start();
     }
