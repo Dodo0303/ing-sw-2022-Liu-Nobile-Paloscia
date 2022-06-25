ebe625f259050a4aefa13870ab2a7aa9bd0735b8
ihsinliu <ihsin.liu@outlook.com>
2022-06-26 01:31:30 +0200

    debug

--- src/main/java/it/polimi/ingsw/Network/Messages/ConnectionStatusMessage.java
+++ src/main/java/it/polimi/ingsw/Network/Messages/ConnectionStatusMessage.java
@@ -7,19 +7,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ConnectionStatusMessage implements Serializable {
-    public final int playerID;
 
     /**
      * True if the player has just connected; false if the player
      * has just disconnected.
      */
-    public final boolean connecting;
-
-    public final List<ClientHandler> players;
 
     public ConnectionStatusMessage(int playerID, boolean connecting, List<ClientHandler> players) {
-        this.playerID = playerID;
-        this.connecting = connecting;
-        this.players = players;
     }
 }
