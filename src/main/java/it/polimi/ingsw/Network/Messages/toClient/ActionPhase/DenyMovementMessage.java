cdb3f53067ec5c2ea2250bf4068f669bcc55240d
ihsinliu <ihsin.liu@outlook.com>
2022-06-27 22:26:00 +0200

    add details to DenyMovementMessage.processGUI

--- src/main/java/it/polimi/ingsw/Network/Messages/toClient/ActionPhase/DenyMovementMessage.java
+++ src/main/java/it/polimi/ingsw/Network/Messages/toClient/ActionPhase/DenyMovementMessage.java
@@ -12,7 +12,7 @@ public class DenyMovementMessage extends MessageToClient {
     public void process(ServerHandler client) {
         CLI cliClient = (CLI) client.getClient();
         if (cliClient.getCurrPhase().equals(Phase.Action2)) {
-            System.out.print("Your movement has been denied, please try again.\n");
+            System.out.print("Island too far away, please try again.\n");
             cliClient.moveMotherNature();
         } else if (cliClient.getCurrPhase().equals(Phase.Action3)) {
             System.out.print("Your movement has been denied, please try again.\n");
@@ -35,13 +35,17 @@ public class DenyMovementMessage extends MessageToClient {
         if (!guiClient.getCurrPhase().equals(Phase_GUI.Planning) &&
                 !guiClient.getCurrPhase().equals(Phase_GUI.Action1) &&
                 !guiClient.getCurrPhase().equals(Phase_GUI.Action2) &&
-                !guiClient.getCurrPhase().equals(Phase_GUI.Action3)) {
+                !guiClient.getCurrPhase().equals(Phase_GUI.Action3)) {//character phases
             guiClient.setCurrPhase(guiClient.getPrevPhase());
             guiClient.setCurrCharacter(-1);
             guiClient.viewSchoolBoard("Character not available", false);
         } else {
             if (guiClient.getCurrPhase().equals(Phase_GUI.Planning)) {
-                guiClient.playAssistant("Assistant not available.");
+                guiClient.playAssistant("Assistant not available");
+            } else if (guiClient.getCurrPhase().equals(Phase_GUI.Action2)){
+                guiClient.checkBoard("Island too far away, please try again");
+            } else if (guiClient.getCurrPhase().equals(Phase_GUI.Action3)){
+                guiClient.checkBoard("Choose a cloud");
             } else {
                 guiClient.viewSchoolBoard("Movement denied", false);
             }
